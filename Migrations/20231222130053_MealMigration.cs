0c33e747e729de2de48332bf1ed6eb0dffdc1473
Mehmet Solak <solakmehmet02@gmail.com>
2024-02-10 18:23:24 +0300

    migrations removed from .gitignore

---
+++ Migrations/20231222130053_MealMigration.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechroseDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class MealMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "blood_sugar_value",
+                table: "users",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "total_dose_value",
+                table: "users",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "weight",
+                table: "users",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "meal_id",
+                table: "user_nutritions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "meals",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    meal_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    total_calorie = table.Column<double>(type: "float", nullable: false),
+                    total_carbohydrate = table.Column<double>(type: "float", nullable: false),
+                    total_sugar = table.Column<double>(type: "float", nullable: false),
+                    blood_sugar = table.Column<double>(type: "float", nullable: false),
+                    meal_time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_meals", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_nutritions_meal_id",
+                table: "user_nutritions",
+                column: "meal_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_user_nutritions_meals_meal_id",
+                table: "user_nutritions",
+                column: "meal_id",
+                principalTable: "meals",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_user_nutritions_meals_meal_id",
+                table: "user_nutritions");
+
+            migrationBuilder.DropTable(
+                name: "meals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_user_nutritions_meal_id",
+                table: "user_nutritions");
+
+            migrationBuilder.DropColumn(
+                name: "blood_sugar_value",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "total_dose_value",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "weight",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "meal_id",
+                table: "user_nutritions");
+        }
+    }
+}
---
+++ Migrations/20231224093122_MealNamesCodesMigration.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechroseDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class MealNamesCodesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "meal_name",
+                table: "meals");
+
+            migrationBuilder.AddColumn<int>(
+                name: "meal_name_code",
+                table: "meals",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "meal_names_codes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    meal_name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_meal_names_codes", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_meals_meal_name_code",
+                table: "meals",
+                column: "meal_name_code");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_meals_meal_names_codes_meal_name_code",
+                table: "meals",
+                column: "meal_name_code",
+                principalTable: "meal_names_codes",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_meals_meal_names_codes_meal_name_code",
+                table: "meals");
+
+            migrationBuilder.DropTable(
+                name: "meal_names_codes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_meals_meal_name_code",
+                table: "meals");
+
+            migrationBuilder.DropColumn(
+                name: "meal_name_code",
+                table: "meals");
+
+            migrationBuilder.AddColumn<string>(
+                name: "meal_name",
+                table: "meals",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
